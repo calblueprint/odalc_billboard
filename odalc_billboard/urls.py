019f30dcf67830d63707e5f5dc35f5a47564d481
akakitani <atsunori.kakitani@gmail.com>
2014-05-17 13:40:11 -0700

    Set up index page template

--- odalc_billboard/urls.py
+++ odalc_billboard/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'odalc_billboard.views.home', name='home'),
-    # url(r'^odalc_billboard/', include('odalc_billboard.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 )
