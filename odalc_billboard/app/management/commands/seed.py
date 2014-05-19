d51e4014d378e61d9b756666959bb129333d837c
akakitani <atsunori.kakitani@gmail.com>
2014-05-18 21:32:15 -0700

    Improve seed file

--- odalc_billboard/app/management/commands/seed.py
+++ odalc_billboard/app/management/commands/seed.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import random
+
 from django.core.management.base import BaseCommand
 
 from odalc_billboard.app.models import Submission
@@ -13,12 +16,12 @@ class Command(BaseCommand):
     md = ModelDataHelper()
 
     def handle(self, *args, **options):
-        instances = 20
+        instances = 50
         for x in range(instances):
-            submission = Submission(
+            submission = Submission.objects.create(
                 content=self.sd.sentence(),
                 points=self.sd.int(min_value=-20, max_value=20),
-                submitted=self.sd.past_datetime(0, 10000)
             )
+            submission.submitted = datetime(random.choice(range(2000, 2013)), random.choice(range(1,12)), random.choice(range(1,28)))
             submission.save()
         print 'Generating submissions...'
